475e45f093811b489cd5710d9eab1997ad04146b
coolCicada <1475395755@qq.com>
2023-05-24 13:33:28 +0800

    feat: 复习变量、类型

--- goByExample/2.Values.go
+++ goByExample/2.Values.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	// 字符串可以通过 + 连接
 	fmt.Println("go" + "lang")
-
+	// 整数和浮点数
 	fmt.Println("1 + 1 = ", 1 + 1)
-	fmt.Println("7.0 / 3.0 = ", 7.0 / 3.0)
-	fmt.Println("7 / 3 = ", 7 / 3)
-	fmt.Println("7.0 / 3 = ", 7.0 / 3)
-	fmt.Println("7 / 3.0 = ", 7 / 3.0)
-
+	fmt.Println("7 / 3 =", 7 / 3)
+	fmt.Println("7.0 / 3.0 =", 7.0 / 3.0)
+	fmt.Println("7.0 / 3 =", 7.0 / 3)
+	fmt.Println("7 / 3.0 =", 7 / 3.0)
+	// 布尔型，以及常见的布尔操作
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
